13d54f91c0d305d479f26faebbf0221b73152ce3
KeBuH <tretyakov.work@inbox.ru>
2018-10-20 22:15:27 +0300

    Revert "Added comment"
    This reverts commit b0d6b6ff1b31655e10638e477931e61a96e8e8b2.

--- src/test/java/ru/tretyakov/counter/CountTest.java
+++ src/test/java/ru/tretyakov/counter/CountTest.java
@@ -5,9 +5,6 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-/**
- * TODO comment.
- */
 public class CountTest {
     /**
      * Класс описывает нить со счетчиком.
